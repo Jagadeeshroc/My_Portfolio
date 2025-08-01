daa179be6cc606d320e58b458498c5fac1a0baa8
jagadeesh <jagadeeshvanganooru@gmail.com>
2025-08-01 10:19:56 +0530

    updated skills in projects

--- src/components/ProjectsPage/ProjectData.jsx
+++ src/components/ProjectsPage/ProjectData.jsx
@@ -3,7 +3,7 @@ export const projects = [
     id: "ecommerce",
     title: "E-commerce Platform",
     description: "A full-featured online store with product listings, cart functionality, and secure checkout process.",
-    technologies: ["React", "Node.js", "MongoDB", "Stripe API", "Tailwind CSS"],
+    technologies: ["React", "Node.js", "MongoDB",  "Tailwind CSS"],
     images: [
       "https://images.unsplash.com/photo-1555529669-e69e7aa0ba9a",
       "https://images.unsplash.com/photo-1486401899868-0e435ed85128",
@@ -27,7 +27,7 @@ export const projects = [
     id: "task-manager",
     title: "Task Management App",
     description: "A productivity application for organizing tasks with drag-and-drop functionality.",
-    technologies: ["React", "Firebase", "Redux", "Material UI"],
+    technologies: ["React","Redux", "Material UI"],
     video: "",
     images: [
       "https://cdn.prod.website-files.com/60c0cec90f57824353f55893/629897cfc77f0da4e04396c7_Task%20Management%20Blog%20(1).png",
@@ -44,7 +44,7 @@ export const projects = [
     id: "Jobby-Application",
     title: "Achyuta Platform Future of Hiring(jobby App) ",
     description: "A productivity application for organizing tasks with drag-and-drop functionality.",
-    technologies: ["React", "Firebase", "Redux", "Material UI"],
+    technologies: ["React", "Redux", "Material UI"],
     video: "",
     images: [
       "https://images.ui8.net/uploads/detail-3_1671703364137.png",
@@ -62,7 +62,7 @@ export const projects = [
     id: "call-manager",
     title: "calls Management App",
     description: "A productivity application for organizing tasks with drag-and-drop functionality.",
-    technologies: ["React", "Firebase", "Redux", "Material UI"],
+    technologies: ["React","Redux", "Material UI"],
     video: "",
     images: [
       "https://cdn.techjockey.com/blog/wp-content/uploads/2021/01/15-Best-Sales-Call-Management-Apps-for-Android-iPhone_-banner.jpg?d=840",
@@ -80,7 +80,7 @@ export const projects = [
   "id": "whatsapp-clone",
   "title": "WhatsApp Web Clone",
   "description": "A real-time messaging application with WhatsApp-like functionality including chat, media sharing, and read receipts.",
-  "technologies": ["React", "Firebase", "Node.js", "Socket.io", "Tailwind CSS"],
+  "technologies": ["React", "Node.js", "Socket.io", "Tailwind CSS"],
   "video": "",
   "images": [
     "/assets/images.png",
